2a6a9984ab0255881cabfcf33adad5e15454c1bf
lukailun <lukailun@mampod.com>
2026-03-17 11:49:20 +0800

    chore: update dependencies

--- src/hooks/useAnimatedHeaderFlatListAnimatedStyles.ts
+++ src/hooks/useAnimatedHeaderFlatListAnimatedStyles.ts
@@ -1,5 +1,5 @@
 import { useHeaderHeight } from '@react-navigation/elements';
-import { useCallback, useState, useEffect } from 'react';
+import { useCallback, useState } from 'react';
 import {
   useWindowDimensions,
   type LayoutRectangle,
@@ -10,7 +10,6 @@ import {
   useAnimatedScrollHandler,
   useAnimatedStyle,
   useSharedValue,
-  cancelAnimation,
   type AnimatedStyle,
   type ScrollHandlerProcessed,
 } from 'react-native-reanimated';
@@ -209,20 +208,6 @@ export const useAnimatedHeaderFlatListAnimatedStyles = ({
         ? 1
         : 0;
   });
-
-  useEffect(() => {
-    return () => {
-      cancelAnimation(scrollY);
-      cancelAnimation(navigationTitleOpacity);
-      cancelAnimation(stickyHeaderOpacity);
-      cancelAnimation(stickyComponentOpacity);
-    };
-  }, [
-    scrollY,
-    navigationTitleOpacity,
-    stickyHeaderOpacity,
-    stickyComponentOpacity,
-  ]);
 
   return {
     scrollHandler,
