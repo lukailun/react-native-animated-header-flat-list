a69cf0ccc9b52d7d7185895e9a3efe7c216341ef
lukailun <lukailun@mampod.com>
2026-06-04 11:33:04 +0800

    feat: add pointerEvents

--- src/components/AnimatedHeaderFlatList.tsx
+++ src/components/AnimatedHeaderFlatList.tsx
@@ -184,6 +184,7 @@ function AnimatedHeaderFlatListInner<T>(
       if (item === HEADER_ITEM) {
         return (
           <View
+            pointerEvents="box-none"
             style={[
               styles.stickyHeaderContainer,
               {
